78a587ba51d120d262a9b4bd4c3401d7d10189ed
SPHINX <corasphinxy@gmail.com>
2021-12-08 13:23:34 -0800

    Complete Design

--- vuejs/src/components/HelloWorld.vue
+++ vuejs/src/components/HelloWorld.vue
@@ -2,30 +2,14 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      You can checkout this project source from github:
+      <a href="https://github.com/corasphinx/Emaww.git" target="_blank" rel="noopener">download source from git</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Export xml to 
+      <a href="https://redis.io/" target="_blank" rel="noopener">Redis</a>.
+    </h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><button v-on:click="exportXML">Export</button></li>
     </ul>
   </div>
 </template>
@@ -35,6 +19,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  // define methods under the `methods` object
+  methods: {
+    exportXML: function () {
+      
+    }
   }
 }
 </script>
@@ -55,4 +45,12 @@ li {
 a {
   color: #42b983;
 }
+button {
+  background-color: #42b983;
+  color: #FFFFFF;
+  width: 200px;
+  height: 50px;
+  border-width: 0px;
+}
+
 </style>
